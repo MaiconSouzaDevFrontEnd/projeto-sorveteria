d22e820192f584cc2ad6e811b4fdffc9f653c1ee
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-10-28 13:02:53 -0300

    criando topo

---
+++ src/app/componentes/Topo/index.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'; 
+import Link from 'next/link';
+import Logo from './public/logo.png';
+import styles from './Topo.module.css';
+
+export default function Topo() {
+    return (
+    <header className={styles.topo}>
+        <Image src={Logo} alt="A imagem de um sorvete" lang='A imagem de um sorvete' />
+        <ul className={styles.menu}>
+            <li>
+                <Link href={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link href={'/sabores'}>Sabores</Link>
+            </li>
+            <li>    
+                <Link href={'/sobre'}>Sobre</Link>
+            </li>
+        </ul>
+    </header>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Topo from "./componentes/Topo";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Topo />
     </main>
   );
 }
