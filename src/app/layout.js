a9de32d9fe65dc0d161e8cd5d1b53f96de775165
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-11-01 21:36:04 -0300

    erro no Logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
 import "./globals.css";
+import Topo from "@/componentes/Topo";
+import Rodape from "@/componentes/Rodape";
 
 export const metadata = {
   title: "Sorveteria Doçura Gelada",
   description: "Sorveteria Doçura Gelada - O melhor sorvete da cidade",
+  robots: "index, follow",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
+        <Topo />
         {children}
+        <Rodape />
       </body>
     </html>
   );
