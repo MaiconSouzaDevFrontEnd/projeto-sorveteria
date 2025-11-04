4a73de872e63e45ef34bbf224a52f02fd2acd569
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-11-04 12:40:52 -0300

    ajustando rodape em preparo

--- src/app/componentes/Rodape/index.jsx
+++ src/app/componentes/Rodape/index.jsx
@@ -8,22 +8,27 @@ export default function Rodape() {
         <footer className={styles.rodape}>
             <div className={styles.containerRodape}>
                 <Image src={Logo} alt="A imagem de um sorvete" className={styles.imgLogo} lang='A imagem de um sorvete' />
-                <div className={styles.subInfos}>
-                    <h3>ENDEREÇO</h3>
-                    <p>Av. Bernardino de Campos, 98 São Paulo, SP 12345-678</p>
-                </div>
-                <div className={styles.subInfos}>
-                    <h3>CONTATO</h3>
-                    <p>info@meusite.com Tel: (11) 3456-7890</p>
-                </div>
-                <div className={styles.subInfos}>
-                    <h3>HORÁRIOS</h3>
-                    <p>ABERTO TODOS OS DIAS 10:00 - 22:00</p>
+                
+                <div className={styles.infosRodape}>
+                    <div className={styles.subInfos}>
+                        <h3>ENDEREÇO</h3>
+                        <p>Av. Bernardino de Campos, 98 São Paulo, SP 12345-678</p>
+                    </div>
+                    <div className={styles.subInfos}>
+                        <h3>CONTATO</h3>
+                        <p>info@meusite.com Tel: (11) 3456-7890</p>
+                    </div>
+                    <div className={styles.subInfos}>
+                        <h3>HORÁRIOS</h3>
+                        <p>ABERTO TODOS OS DIAS 10:00 - 22:00</p>
+                    </div>
                 </div>
             </div>
 
-            <p>© 2025 Sorveteria Doçura Gelada. Todos os direitos reservados.</p>   
-            <p>Orgulhosamente desenvolvido por "Maicon Souza da Silva".</p>
+            <div className={styles.copyRight}>
+                <p>© 2025 Sorveteria Doçura Gelada. Todos os direitos reservados.</p>   
+                <p>Orgulhosamente desenvolvido por "Maicon Souza da Silva".</p>
+            </div>
             
             <Link href="https://www.devmedia.com.br/" target="_blank" className={styles.btnDevmedia} >Todo Crédito à Devmedia</Link>
         </footer>
