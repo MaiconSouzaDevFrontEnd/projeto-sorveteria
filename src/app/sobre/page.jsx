786cab1a836ca0e9c823d3561ab275675fb553c2
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-11-17 21:45:09 -0300

    iniciando page sobre

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,7 +1,40 @@
-export default function Sabores(){
+import Image from  'next/image';
+import styles from './sobre.module.css';
+import Sobre from '../../../public/sobre-image.jpg';
+import Sorveteria from '../../../public/sorveteria.jpg';
+
+export default function Sobre(){
     return(
-        <section>
-            <h1>Teste Sobre</h1>
-        </section>
+        <main>
+            <header className={styles.headerBanner}>
+                <h1 className={styles.titleSobre}>A GELATERIA</h1>
+            </header>
+
+            <section className={styles.containerSobre}>
+                <h2 className={styles.titleSobre}>Sobre Nós</h2>
+
+                <h3 className={styles.titleAmamosSorvete}>Nós simplesmente amamos sorvete!</h3>
+
+                <p className={styles.textoSobre}>
+                    Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. 
+                    Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios.
+                    Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete.
+                    Vendemos tanto para varejo como para atacado.
+                </p>
+                <p className={styles.textoSobre}>
+                    Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar
+                    para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos
+                    eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços,
+                    basta entrar em contato conosco. Iremos proporcionar o melhor atendimento e os melhores produtos para você fazer
+                    a sua festa mais saborosa, com o melhor sorvete da cidade.
+
+                </p>
+            </section>
+
+            <section className={styles.containerImagens}>
+                <Image src={Sobre} alt='pessoas com sorvete com casca na mão, brindando.' className={styles.img}/>
+                <Image src={Sorveteria} alt='Dentro da sorveteria o local.' className={styles.img}/>
+            </section>
+        </main>
     )
 }
